ffa85a096c6521f2e937458dbf85cab345daa62a
Alona Smatko <161590615+alon-asma-tko@users.noreply.github.com>
2024-11-09 13:19:30 +0000

    Added attributes to the Product table and ProductPhotos table of products/config/init.sql

--- server/components/products/config/init.sql
+++ server/components/products/config/init.sql
@@ -7,12 +7,19 @@ CREATE DATABASE Products;
 
 CREATE TABLE Product (
     id SERIAL PRIMARY KEY
-
+    product_name VARCHAR(100) NOT NULL CHECK (char_length(product_name) >=3), /*'product_name' is 'name' on the yaml file.*/
+    category_id INTEGER,
+    category_name VARCHAR(100) NOT NULL CHECK (char_length(category_name) >=3),
+    short_description VARCHAR(1000) NOT NULL CHECK (char_length(short_description) >=20),
+    detailed_description VARCHAR (10000),
+    -- product_photos VARCHAR(1000)[],
+    product_url VARCHAR(1000)
 );
 
 CREATE TABLE ProductPhotos (
-    id SERIAL PRIMARY KEY
-
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER REFERENCES Product(id) ON DELETE CASCADE,
+    photo_url VARCHAR(1000) NOT NULL
 );
 
 -- This table is negotiable dependening on the priorities
