74667a58d18b09d9577d2d7332cd45024420c2e2
Wong Chung Man <84464954+deeplearner1992@users.noreply.github.com>
2024-11-09 02:14:23 +0000

    modified the input and output of endpoints

--- server/components/categories/db/categories.db.js
+++ server/components/categories/db/categories.db.js
@@ -12,7 +12,7 @@ module.exports.getCategoriesDb = async (options) => {
   };
 };
 
-module.exports.postCategoriesDb = async (options) => {
+module.exports.postCategoriesDb = async (title, photo_url, description) => {
   /** Imagine that in this funciton, you will perform the database query and get its output in result: result = await pool.query();
   1- Modify options to be specific parameters or one of your objects: think about what you need to recieve from services to do the query successfully
   2- Thinks about the entities you need to access here. Are they created? are they well defined? Can you make sure entities in init.sql are updated. 
@@ -24,7 +24,7 @@ module.exports.postCategoriesDb = async (options) => {
   };
 };
 
-module.exports.putCategoriesByIdDb = async (options) => {
+module.exports.putCategoriesByIdDb = async (id, title, photo_url, description) => {
   /** Imagine that in this funciton, you will perform the database query and get its output in result: result = await pool.query();
   1- Modify options to be specific parameters or one of your objects: think about what you need to recieve from services to do the query successfully
   2- Thinks about the entities you need to access here. Are they created? are they well defined? Can you make sure entities in init.sql are updated. 
@@ -36,7 +36,7 @@ module.exports.putCategoriesByIdDb = async (options) => {
   };
 };
 
-module.exports.deleteCategoriesByIdDb = async (options) => {
+module.exports.deleteCategoriesByIdDb = async (id) => {
   /** Imagine that in this funciton, you will perform the database query and get its output in result: result = await pool.query();
   1- Modify options to be specific parameters or one of your objects: think about what you need to recieve from services to do the query successfully
   2- Thinks about the entities you need to access here. Are they created? are they well defined? Can you make sure entities in init.sql are updated. 
