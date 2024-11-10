eade0a4d05b5b82ae80f2ef95e7d0a0dca8af3b6
Alberto-IT-23 <albertocozzarin@outlook.com>
2024-11-10 12:10:46 +0100

    Refactor postAdminsRegister function to accept registrationData parameter for improved clarity and functionality

--- server/components/admins/db/admins.db.js
+++ server/components/admins/db/admins.db.js
@@ -96,7 +96,7 @@ module.exports.postAdminsPasswordResetDb = async (options) => {
   };
 };
 
-module.exports.postAdminsRegisterDb = async (options) => {
+module.exports.postAdminsRegisterDb = async (registrationData) => {
   /** Imagine that in this funciton, you will perform the database query and get its output in result: result = await pool.query();
   1- Modify options to be specific parameters or one of your objects: think about what you need to recieve from services to do the query successfully
   2- Thinks about the entities you need to access here. Are they created? are they well defined? Can you make sure entities in init.sql are updated. 
