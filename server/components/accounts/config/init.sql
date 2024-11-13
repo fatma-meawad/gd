13c6de306fc6710d485fdd18cb9bbf098039d862
Ryan Bekkum <ryan.bekkum@students.opit.com>
2024-11-13 07:50:23 -0700

    Update init.sql
    I forgot to remove my db creation for testing. Reverted it back to the original.

--- server/components/accounts/config/init.sql
+++ server/components/accounts/config/init.sql
@@ -1,26 +1,14 @@
--- Drop the database if it already exists, then recreate it
-DROP DATABASE IF EXISTS "accounts";
+CREATE DATABASE Accounts;
 
--- Create the database if it does not exist
-CREATE DATABASE accounts;
+\c Accounts
 
--- Connect to the database
-\c accounts
+-- Don't add tables here. 
 
--- Create the BusinessAccount table with the required attributes for the business endpoint
-CREATE TABLE IF NOT EXISTS BusinessAccount (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    image TEXT,
-    phone VARCHAR(20),
-    address VARCHAR(255) NOT NULL,
-    web_address TEXT,
-    description TEXT,
-    main_owner_name VARCHAR(100) NOT NULL,
-    main_owner_email VARCHAR(100) NOT NULL,
-    main_owner_phone VARCHAR(20) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+-- Add the attributes you need to implement your feature?
+
+CREATE TABLE BusinessAccount (
+    id SERIAL PRIMARY KEY
+
 );
 
 
