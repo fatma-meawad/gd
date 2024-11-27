56eb71203bcb75fa53efc65bf339a5bd84e405ee
Marika Mella <116084393+marikadeveloper@users.noreply.github.com>
2024-11-27 20:06:37 +0000

    Add mock products import inside getProductsDb method for jest mock to work

--- server/components/products/db/products.db.js
+++ server/components/products/db/products.db.js
@@ -1,7 +1,7 @@
 const schema = require("../schema.json");
-const mockProducts = require("./mock/products.json");
 
 module.exports.getProductsDb = async (limit, cursor) => {
+  const mockProducts = require("./mock/products.json");
   /** Imagine that in this funciton, you will perform the database query and get its output in result: result = await pool.query();
   1- Modify options to be specific parameters or one of your objects: think about what you need to recieve from services to do the query successfully
   2- Thinks about the entities you need to access here. Are they created? are they well defined? Can you make sure entities in init.sql are updated. 
