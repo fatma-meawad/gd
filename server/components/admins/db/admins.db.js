84e6e0fffbd033a7b2a8222bdc9a57e5b2e37648
Alberto-IT-23 <albertocozzarin@outlook.com>
2024-11-30 16:56:12 +0100

    Refactor postAdminsRegister to use a structured admin object on db, services, and controller

--- server/components/admins/db/admins.db.js
+++ server/components/admins/db/admins.db.js
@@ -97,7 +97,7 @@ module.exports.postAdminsPasswordResetDb = async (options) => {
   };
 };
 
-  module.exports.postAdminsRegisterDb = async (registrationData) => {
+  module.exports.postAdminsRegisterDb = async (admin) => {
     /** Imagine that in this funciton, you will perform the database query and get its output in result: result = await pool.query();
     1- Modify options to be specific parameters or one of your objects: think about what you need to recieve from services to do the query successfully
     2- Thinks about the entities you need to access here. Are they created? are they well defined? Can you make sure entities in init.sql are updated. 
@@ -106,9 +106,8 @@ module.exports.postAdminsPasswordResetDb = async (options) => {
     return {
     data: {
       id: "admin-123",
-      email: registrationData.email,
-      full_name: registrationData.full_name,
-      created_at: new Date().toISOString(),
+      email: admin.email,
+      full_name: admin.full_name,
     },
     messages: ["postAdminsRegisterDb not implemented yet"],
     locations: ["admins.database.js"],
