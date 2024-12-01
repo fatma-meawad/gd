6fdc08888841b6e72b6e6f6a40fe9a2818311e63
Alberto-IT-23 <albertocozzarin@outlook.com>
2024-12-01 01:12:52 +0100

    Refactor postAdminsRegisterDb function for improved readability and maintainability

--- server/components/admins/db/admins.db.js
+++ server/components/admins/db/admins.db.js
@@ -97,13 +97,13 @@ module.exports.postAdminsPasswordResetDb = async (options) => {
   };
 };
 
-  module.exports.postAdminsRegisterDb = async (admin) => {
-    /** Imagine that in this funciton, you will perform the database query and get its output in result: result = await pool.query();
-    1- Modify options to be specific parameters or one of your objects: think about what you need to recieve from services to do the query successfully
-    2- Thinks about the entities you need to access here. Are they created? are they well defined? Can you make sure entities in init.sql are updated. 
-    3- you can access the schema.json (imported above) and use objects in it/modify or create them.
-  */
-    return {
+module.exports.postAdminsRegisterDb = async (admin) => {
+  /** Imagine that in this funciton, you will perform the database query and get its output in result: result = await pool.query();
+  1- Modify options to be specific parameters or one of your objects: think about what you need to recieve from services to do the query successfully
+  2- Thinks about the entities you need to access here. Are they created? are they well defined? Can you make sure entities in init.sql are updated. 
+  3- you can access the schema.json (imported above) and use objects in it/modify or create them.
+*/
+  return {
     data: {
       id: "123",
       email: admin.email,
