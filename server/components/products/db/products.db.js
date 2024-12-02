bb78df1b6d8fb87fabb95474613d33acee8622be
Alona Smatko <161590615+alon-asma-tko@users.noreply.github.com>
2024-12-02 11:44:22 +0000

    Remove the logic to pass the db unit tests.

--- server/components/products/db/products.db.js
+++ server/components/products/db/products.db.js
@@ -18,50 +18,50 @@ module.exports.postProductsDb = async (product) => {
   2- Thinks about the entities you need to access here. Are they created? are they well defined? Can you make sure entities in init.sql are updated. 
   3- you can access the schema.json (imported above) and use objects in it/modify or create them.
 */
-const {
-  id,
-  product_name,
-  category_id,
-  category_name, 
-  short_description,
-  detailed_description, 
-  product_photos = [], // Default to empty array if missing
-  product_url = ""
-} = product;
+// const {
+//   id,
+//   product_name,
+//   category_id,
+//   category_name, 
+//   short_description,
+//   detailed_description, 
+//   product_photos = [], // Default to empty array if missing
+//   product_url = ""
+// } = product;
 
 //Check for missing fields (To pass Test Case 2: Invalid input - missing required field/-s)
-const requiredFields = ['category_id', 'product_name', 'short_description'];
-const missingFields = requiredFields.filter(field => !product[field]);
+// const requiredFields = ['category_id', 'product_name', 'short_description'];
+// const missingFields = requiredFields.filter(field => !product[field]);
 
-if (missingFields.length > 0) {
-  throw new Error(`Missing required fields: ${missingFields.join(', ')}`);
-}
+// if (missingFields.length > 0) {
+//   throw new Error(`Missing required fields: ${missingFields.join(', ')}`);
+// }
 
 //Validate data types (To pass Test Case 3: Invalid input - incorrect data types/-s)
-if (
-  typeof category_id !== 'number' ||
-  typeof product_name !== 'string' ||
-  typeof short_description !== 'string'
-  ) 
-{throw new Error('Invalid data types entered');}
+// if (
+//   typeof category_id !== 'number' ||
+//   typeof product_name !== 'string' ||
+//   typeof short_description !== 'string'
+//   ) 
+// {throw new Error('Invalid data types entered');}
 
-const newProduct = {
-  id,
-  product_name,
-  category_id,
-  category_name,
-  short_description,
-  detailed_description,
-  product_photos,
-  product_url,
-};
+// const newProduct = {
+//   id,
+//   product_name,
+//   category_id,
+//   category_name,
+//   short_description,
+//   detailed_description,
+//   product_photos,
+//   product_url,
+// };
 
 //Simulate saving the product by pushing it into the mock array
-mockProducts.push(newProduct);
+// mockProducts.push(newProduct);
 
 
   return {
-    data: newProduct,
+    // data: newProduct,
     //**Swagger shows error 500 if the below returns are not present. But postProductsDb unit tests only pass without them.**
     messages: ["postMessagesDb not implemented yet"],
     locations: ["messages.database.js"],
