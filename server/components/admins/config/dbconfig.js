02a8e31b799d81bc99623c1fc9ef44d058008e00
Fatma <felsayedmeawad@gmail.com>
2024-12-03 08:53:57 +0000

    updating all local environment files and dbconfig files to get ready for db access

--- server/components/admins/config/dbconfig.js
+++ server/components/admins/config/dbconfig.js
@@ -2,26 +2,22 @@ const { Pool } = require("pg");
 const path = require("path");
 require("dotenv-flow").config({ path: path.resolve(__dirname) });
 
-console.log(process.env.NODE_ENV);
-console.log(process.env.POSTGRES_DB_TEST);
-
 const database =
   process.env.NODE_ENV === "test"
-    ? process.env.POSTGRES_DB_TEST
-    : process.env.POSTGRES_DB;
+    ? process.env.POSTGRES_ADMIN_DB_TEST
+    : process.env.POSTGRES_ADMIN_DB;
 
 const user =
   process.env.NODE_ENV === "test"
-    ? process.env.POSTGRES_USER_TEST
-    : process.env.POSTGRES_USER;
+    ? process.env.POSTGRES_ADMIN_USER_TEST
+    : process.env.POSTGRES_ADMIN_USER;
 
 const password =
   process.env.NODE_ENV === "test"
-    ? process.env.POSTGRES_PASSWORD_TEST
-    : process.env.POSTGRES_PASSWORD;
+    ? process.env.POSTGRES_ADMIN_PASSWORD_TEST
+    : process.env.POSTGRES_ADMIN_PASSWORD;
 
 const connectionString = `postgresql://${user}:${password}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${database}`;
-console.log(connectionString);
 const pool = new Pool({ connectionString });
 
 module.exports = {
