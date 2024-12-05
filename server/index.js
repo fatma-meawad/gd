c8eaac56684ec196999250d2bc8847e0d86cdab0
Fatma <felsayedmeawad@gmail.com>
2024-12-05 06:16:27 +0000

    turning 5000 into a const

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ require("dotenv").config({ path: __dirname + "/.env" });
 const http = require("http");
 const { clogger } = require("./utils/logger");
 const app = require("./app");
+const DEFAULT_PORT=5000;
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || DEFAULT_PORT;
 server.listen(PORT, () => clogger.info(`Awesome API running on port: ${PORT}`));
