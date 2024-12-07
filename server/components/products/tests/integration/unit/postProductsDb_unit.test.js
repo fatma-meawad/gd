ea6375f351dfce964bd7a708504f8184f566f648
Alona Smatko <161590615+alon-asma-tko@users.noreply.github.com>
2024-12-07 13:27:10 +0000

    Update postProductsDb_unit.test.js by changing path to Mock, changing expected properties for successful product, as well commenting out the categoty_name and product_photos parameters for now, as these will be potentially worked on later.

--- server/components/products/tests/integration/unit/postProductsDb_unit.test.js
+++ server/components/products/tests/integration/unit/postProductsDb_unit.test.js
@@ -1,19 +1,20 @@
+require("dotenv-flow").config();//added at phase 2
 const { postProductsDb } = require("/workspaces/galleria-dashboard/server/components/products/db/products.db");
-const mockProducts = require("/workspaces/galleria-dashboard/server/components/products/db/mock/products.json");
+const mockProducts = require("/workspaces/galleria-dashboard/server/components/products/tests/unit/mock/products.json");
 
 describe("postProductsDb", () => {
-  jest.mock("/workspaces/galleria-dashboard/server/components/products/db/mock/products.json", () => mockProducts);
+  jest.mock("/workspaces/galleria-dashboard/server/components/products/tests/unit/mock/products.json", () => mockProducts);
 
     const validProduct = {
         id: 123,
         product_name: "RGB keyboard",
         category_id: 12345,
-        category_name: "Keyboards",
+        // category_name: "Keyboards",
         short_description: "Experience seamless typing with this wireless RGB keyboard, featuring customizable backlighting, ergonomic design, and reliable connectivity for a smooth and vibrant typing experience",
         detailed_description: "Upgrade your typing experience with this wireless RGB keyboard, combining style and functionality. With customizable RGB lighting, an ergonomic design, and quiet, responsive keys, it ensures a smooth typing experience whether you're working or gaming. Benefit from stable wireless connectivity, extended battery life, and a sleek, vibrant design that enhances any workspace",
-        product_photos: [
-            "https://example.com/photo1.jpg",
-            "https://example.com/photo2.jpg"],
+        // product_photos: [
+        //     "https://example.com/photo1.jpg",
+        //     "https://example.com/photo2.jpg"],
         product_url: "https://youtu.be/0LE0trd740Q"
         };
 
@@ -21,12 +22,12 @@ describe("postProductsDb", () => {
         id: 123,
         product_name: "RGB keyboard",
         category_id: "12345",
-        category_name: "Keyboards",
+        // category_name: "Keyboards",
         short_description: "Experience seamless typing with this wireless RGB keyboard, featuring customizable backlighting, ergonomic design, and reliable connectivity for a smooth and vibrant typing experience",
         detailed_description: "Upgrade your typing experience with this wireless RGB keyboard, combining style and functionality. With customizable RGB lighting, an ergonomic design, and quiet, responsive keys, it ensures a smooth typing experience whether you're working or gaming. Benefit from stable wireless connectivity, extended battery life, and a sleek, vibrant design that enhances any workspace",
-        product_photos: [
-            "https://example.com/photo1.jpg",
-            "https://example.com/photo2.jpg"],
+        // product_photos: [
+        //     "https://example.com/photo1.jpg",
+        //     "https://example.com/photo2.jpg"],
         product_url: "https://youtu.be/0LE0trd740Q"
         };
 
@@ -39,19 +40,12 @@ describe("postProductsDb", () => {
     it("should successfully save the product", async () => {
         const result = await postProductsDb(validProduct);
 
-        expect(result).toEqual({
-        data: {
-            id: validProduct.id,
-            product_name: validProduct.product_name,
-            category_id: validProduct.category_id,
-            category_name: validProduct.category_name,
-            short_description: validProduct.short_description,
-            detailed_description: validProduct.detailed_description,
-            product_photos: validProduct.product_photos,
-            product_url: validProduct.product_url
-        },
-        });
-        expect(mockProducts).toContainEqual(validProduct);
+        expect(result.data).toHaveProperty('product_name', validProduct.product_name);
+        expect(result.data).toHaveProperty('category_id', validProduct.category_id);
+        expect(result.data).toHaveProperty('short_description', validProduct.short_description);
+        expect(result.data).toHaveProperty('detailed_description', validProduct.detailed_description);
+        expect(result.data).toHaveProperty('product_url', validProduct.product_url);
+
     });
 
     //Test Case 2: Invalid input - missing required field/-s
